250e224b655b112f78636b710d430188185aa42c
Andrea Vaiuso <andreavaiuso@community.unipa.it>
2021-10-25 17:14:26 +0200

    PKSpot 1.0

--- pkspot.py
+++ pkspot.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import sys
+import numpy as np
 import xml.etree.ElementTree as ET
 
 from tensorflow.python.eager.context import num_gpus
@@ -9,12 +10,18 @@ from utilities import printErr
 
 CLASSIFIER = "NETWORK/malexcnr.keras"
 IMG_FORMAT = ".jpg"
+TOL = 20
 
 class Patch:
-    def  __init__(self,name,imgArray):
+    def  __init__(self,name,imgArray,imsource,x1,x2,y1,y2):
        self.name = name
        self.imgArray = imgArray
        self.confidence = 1
+       self.imsource = imsource
+       self.x1 = x1
+       self.x2 = x2
+       self.y1 = y1
+       self.y2 = y2
 
 def getFName(name):
     n = name.split(".")
@@ -66,6 +73,27 @@ def weightedMean(val,w):
         den += w[i]
     return num/den
 
+def setConfidence(patches,camname,parkname,factor):
+    for patchlist in patches:
+        confs = []
+        alpha = 0
+        
+        if patchlist[0].name != parkname:
+            continue
+        for patch in patchlist:
+            if getFName(patch.imsource) != camname:
+                confs.append(patch.confidence)
+                continue
+            alpha = patch.confidence
+        if len(confs) == 0:
+            return patches
+        nfac = (1 - (factor * alpha)) / sum(confs)
+        for patch in patchlist:
+            if getFName(patch.imsource) != camname:
+                patch.confidence = patch.confidence*nfac
+            else: patch.confidence = patch.confidence*factor
+    return patches
+
 def main():
     #RULES! The name of XML file must match the name of image file
     xmlpath = sys.argv[1]
@@ -88,11 +116,11 @@ def main():
         xmlRoots.append(ET.parse(xmlpath+"/"+xmlfile).getroot())
     for imgfile in imgdirs:
         imgArrays.append(cv2.imread(imgpath+"/"+imgfile))
+
     xmlRoots, imgArrays = orderArrays(xmlRoots,xmldirs,imgArrays,imgdirs)
 
     patches = []
 
-    i = 0
     for i in range(ln):
         for child in xmlRoots[i]:
             patchname = ""
@@ -111,9 +139,18 @@ def main():
                     y1 = int(patch.text)
                 if patch.tag == "Y2":
                     y2 = int(patch.text)
-            ptc = Patch(patchname,imgArrays[i][y1:y2,x1:x2])
+            ptc = Patch(patchname,imgArrays[i][y1:y2,x1:x2],imgdirs[i],x1,x2,y1,y2)
             patches = addToPatches(patches,ptc)
-    i += 1
+
+    patches = setConfidence(patches,"CAM3","Parking 8", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 9", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 10", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 11", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 12", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 13", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 14", 0.1)
+    patches = setConfidence(patches,"CAM3","Parking 15", 0.1)
+
 
     classifier.loadModel(CLASSIFIER)
 
@@ -132,10 +169,52 @@ def main():
             classifResult = "{:.2f}% ".format(abs(pred)) + result
             tempResult.append(pred)
             confs.append(patch.confidence)
+            print(patch.name,"-",patch.imsource,":",classifResult,"[",patch.confidence,"]")
         predResult.append(weightedMean(tempResult,confs))
-    
-    print(predResult)
+    print()
+    i = 0
+    for res in predResult:
+        result = ""
+        if res > 0: result = "free"
+        else: result = "occupied"
+        classifResult = "{:.2f}% ".format(abs(res)) + result
+        print(patches[i][0].name,classifResult,"[",str(len(patches[i])),"cameras ]")
+        i += 1
+    showOverlays(imgdirs,patches,predResult)
 
+def showOverlays(imgdirs,patches,preds):
+    red_color = (0, 0, 255, 255)
+    green_color = (0, 255, 0, 255)
+    white_color = (255, 255, 255, 255)
+    
+    imgs = []
+    imgpath = sys.argv[2]
+    for imgfile in imgdirs:
+        rgb_data = cv2.imread(imgpath+"/"+imgfile)
+        rgba = cv2.cvtColor(rgb_data, cv2.COLOR_RGB2RGBA)
+        imgs.append(rgba)
+    
+    i = 0
+    for i in range(len(imgs)):
+        img = imgs[i]
+        x,y,ch = img.shape
+        overlay = np.zeros((x,y,4))
+        ptcdex = 0
+        for ptc in patches:
+            for patch in ptc:
+                if patch.imsource == imgdirs[i]:
+                    result = preds[ptcdex]
+                    if result>TOL: color = green_color
+                    elif result<-TOL: color = red_color
+                    else: color = white_color
+                    overlay = cv2.rectangle(overlay,(patch.x1,patch.y1),(patch.x2,patch.y2),color,3) 
+            ptcdex +=1
+        res = imgs[i]
+        cnd = overlay[:, :, 3] > 0
+        res[cnd] = overlay[cnd]
+        cv2.imshow("Overlay " + imgdirs[i],res)
+        cv2.waitKey()
+        i += 1
 
 if __name__ == "__main__":
     main()
